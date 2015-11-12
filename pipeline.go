eb931e179df741e3563476bddc944e5c56f0016e
Jin Zhou <ferryzhou@gmail.com>
2015-11-12 11:54:22 -0500

    added initial bqclient code

---
+++ pipeline.go
@@ -0,0 +1,83 @@
+// package pipeline contains tools to
+//  1. move data from bq to local.
+//  2. run queries on bq to extract data
+//  3. store dumped data to database
+package pipeline
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/bigquery/v2"
+)
+
+const (
+	bqEndpoint = "https://www.googleapis.com/auth/bigquery"
+)
+
+type bqclient struct {
+	service *bigquery.Service
+
+	projectId string
+	datasetId string
+}
+
+func getBigqueryService(pemPath string) (*bigquery.Service, error) {
+	// generate auth token and create service object
+	pemKeyBytes, err := ioutil.ReadFile(pemPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read pem file: %s, error: %v", pemPath, err)
+	}
+
+	conf, err := google.JWTConfigFromJSON(pemKeyBytes, bqEndpoint)
+
+	client := conf.Client(oauth2.NoContext)
+
+	return bigquery.New(client)
+}
+
+func NewBQClient(pemPath string) (*bqclient, error) {
+	c := &bqclient{}
+	service, err := getBigqueryService(pemPath)
+	if err != nil {
+		return nil, err
+	}
+	c.service = service
+	return c, nil
+}
+
+func (c *bqclient) getQueryJob(query, dstTable string) (*bigquery.Job, error) {
+	dstTableRef := &bigquery.TableReference{
+		ProjectId: c.projectId,
+		DatasetId: c.datasetId,
+		TableId:   dstTable,
+	}
+	defaultDatasetRef := &bigquery.DatasetReference{
+		ProjectId: c.projectId,
+		DatasetId: c.datasetId,
+	}
+	qConf := &bigquery.JobConfigurationQuery{
+		Query:             query,
+		DestinationTable:  dstTableRef,
+		DefaultDataset:    defaultDatasetRef,
+		AllowLargeResults: true,
+		WriteDisposition:  "WRITE_TRUNCATE",
+		CreateDisposition: "CREATE_IF_NEEDED",
+		FlattenResults:    flatten,
+	}
+	conf := &bigquery.JobConfiguration{
+		Query: qConf,
+	}
+
+	return &bigquery.Job{
+		Configuration: conf,
+	}, nil
+}
+
+func bqexport(sql string, path string) error {
+}
